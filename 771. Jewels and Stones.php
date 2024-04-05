4b0236f469e179060ff9117e27ff4feaf63c0548
vtyurin89 <vlad89main@gmail.com>
2024-04-05 14:20:38 +0300

    ADDED 771 jewels and stones

--- 771. Jewels and Stones.php
+++ 771. Jewels and Stones.php
@@ -2,13 +2,18 @@
 
 class Solution {
 
-/**
- * @param String $jewels
- * @param String $stones
- * @return Integer
- */
-function numJewelsInStones($jewels, $stones) {
-    
-}
-}
-
+    /**
+     * @param String $jewels
+     * @param String $stones
+     * @return Integer
+     */
+    function numJewelsInStones($jewels, $stones) {
+        $res = 0;
+        for($i=0; $i < strlen($stones); $i++) {
+            if (array_key_exists(ord($stones[$i]), count_chars($jewels, 1))) {
+                $res += 1;
+            }
+        }
+        return $res;
+    }
+    }
