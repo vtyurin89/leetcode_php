05ee9e774bb7f5ef4e421ba8a0759eaa7558f542
vtyurin89 <vlad89main@gmail.com>
2024-04-08 15:57:29 +0300

    ADDED 258 add digits

--- 258. Add Digits.php
+++ 258. Add Digits.php
@@ -2,11 +2,23 @@
 
 class Solution {
 
-/**
- * @param Integer $num
- * @return Integer
- */
-function addDigits($num) {
+    /**
+     * @param Integer $num
+     * @return Integer
+     */
+    function addDigits($num) {
+        while($num > 9) {
+            $sum = 0;
+            foreach (str_split(strval($num)) as $num_char) {
+                $sum += $num_char;
+            }
+            $num = $sum;
+        }
+        return $num;
+    }
+    }
     
-}
-}
+    $sol = new Solution();
+    assert($sol -> addDigits(0) == 0);
+    assert($sol -> addDigits(0) == 0);
+    
