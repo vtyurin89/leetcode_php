fe31cd55c9184ce7d3d616b821799e0a3af28cad
vtyurin89 <vlad89main@gmail.com>
2024-04-15 15:14:29 +0300

    ADDED 905 sort array by purity

---
+++ 905. Sort Array By Parity.php
@@ -0,0 +1,25 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function sortArrayByParity($nums) {
+        $even = array();
+        $odd = array();
+        foreach($nums as $num) {
+        	if ($num % 2 == 0) {
+        		$even[] = $num;
+        	} else {
+        		$odd[] = $num;
+        	}
+        }
+        $result = array_merge($even, $odd);
+        return $result;
+    }
+}
+
+$sol = new Solution();
+$sol -> sortArrayByParity([3,1,2,4]);
